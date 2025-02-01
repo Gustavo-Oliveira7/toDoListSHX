62931651ec0e054bae70b21d1d01663f504708a3
Gustavo-Oliveira <gustavo.h.oliveira7531@gmail.com>
2025-02-01 11:07:33 -0300

    fetch an api

--- components/Task.js
+++ components/Task.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
+const linkTodo = 'https://jsonplaceholder.typicode.com/todos';
+
 const Task = () => {
+
+    const [isLoading,setLoading] = useState(true)
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch(linkTodo)
+            .then((response) => response.json())
+                .then((data) => setData(data.slice(0,4)))
+                    .catch((error) => alert(error))
+                        .finally(setLoading(false))
+    })
+
+
     return(
-        <View>
+        <View>           
             <Text>
-                task example
+                {isLoading ? <Text>loading</Text> : <Text>done</Text>}
             </Text>
         </View>
     )
