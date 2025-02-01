5dfd61a5a74710a672f0a7bdf5322c032fe0b60d
Gustavo-Oliveira <gustavo.h.oliveira7531@gmail.com>
2025-02-01 10:20:43 -0300

    creating a task component

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>
+          Today
+        </Text>
+        <Text>
+          <Task/>
+        </Text>
+      </View>
+
+
     </View>
   );
 }
